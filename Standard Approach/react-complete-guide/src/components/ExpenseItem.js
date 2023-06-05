9466e8782fab7a0465d32beafaaa59aa95aac6a7
Shashwat Singh <shashwat2048@gmail.com>
2023-06-05 18:41:00 +0530

    36. Passing data via props to build multiple dynamic components

--- Standard Approach/react-complete-guide/src/components/ExpenseItem.js
+++ Standard Approach/react-complete-guide/src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseData = new Date(2023, 6, 5);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 299.98;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseData.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
