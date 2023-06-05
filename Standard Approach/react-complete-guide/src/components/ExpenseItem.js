9fe91a508ac827ae19b85f9c439e2307a8458e8c
Shashwat Singh <shashwat2048@gmail.com>
2023-06-05 16:59:18 +0530

    till 34 made a component, added few data and styling

--- Standard Approach/react-complete-guide/src/components/ExpenseItem.js
+++ Standard Approach/react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div>June 5th, 2023</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$299.98</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
