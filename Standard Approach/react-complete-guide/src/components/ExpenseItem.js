91ef853f00a6b3428302ddce8a564a8a05189e57
Shashwat Singh <shashwat2048@gmail.com>
2023-06-05 17:16:42 +0530

    35. Adding function so that data is not hard coded inside JSX code and could be added dynamically.

--- Standard Approach/react-complete-guide/src/components/ExpenseItem.js
+++ Standard Approach/react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseData = new Date(2023, 6, 5);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 299.98;
+
   return (
     <div className="expense-item">
-      <div>June 5th, 2023</div>
+      <div>{expenseData.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$299.98</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
