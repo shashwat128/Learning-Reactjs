f8c8c1dda9333618526e1ac0d54e213d0bde6714
Shashwat Singh <shashwat2048@gmail.com>
2023-06-06 13:32:34 +0530

    38. Assignment

--- Standard Approach/react-complete-guide/src/App.js
+++ Standard Approach/react-complete-guide/src/App.js
@@ -1,7 +1,7 @@
 import "./components/ExpenseItem";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-function App() {
+function App(props) {
   const expenses = [
     {
       id: "e1",
@@ -31,27 +31,8 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <h2>Let's get started.</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
