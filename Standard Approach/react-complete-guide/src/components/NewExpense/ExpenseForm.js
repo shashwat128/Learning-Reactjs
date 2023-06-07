5ac3bd2d417618b000e678f47171c7f90fccdad1
Shashwat Singh <shashwat2048@gmail.com>
2023-06-07 12:01:05 +0530

    52.53. Listening to user input and working with multiple sets

--- Standard Approach/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ Standard Approach/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2010-01-01" max="2023-01-01" />
+          <input
+            type="date"
+            min="2010-01-01"
+            max="2023-01-01"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
