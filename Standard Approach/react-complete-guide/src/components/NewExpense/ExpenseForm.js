347cd2096877600028bc4d6ad465ef44f61c3852
Shashwat Singh <shashwat2048@gmail.com>
2023-06-07 12:32:32 +0530

    54.55. Using single 'useState()' approach and storing value of previous state

--- Standard Approach/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ Standard Approach/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -7,6 +7,35 @@ const ExpenseForm = () => {
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
+  /*
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enetredDate: '',
+  });
+
+  const titleChangeHandler = (event) => {
+    setUserInput({
+        ...userInput,
+        enteredTitle: event.target.value,
+        });
+    };
+
+    const amountChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredAmount: event.targer.value,
+        });
+    };
+
+    const dateChangeHandler = (event) => {
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value,
+        });
+    };
+   */
+
   const titleChangeHandler = (event) => {
     console.log(event.target.value);
   };
