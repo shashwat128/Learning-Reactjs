2978f942a5de2ebf8538e28cacf3b6408471604f
Shashwat Singh <shashwat2048@gmail.com>
2023-06-08 13:37:26 +0530

    Assignment 3 solution - solution of instructor not compatible with new React environment

--- Standard Approach/react-complete-guide/src/components/Expenses/Expenses.js
+++ Standard Approach/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
-
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
 
-const Expenses = (props) => {
+function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <div>
@@ -19,18 +22,21 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
       </Card>
     </div>
   );
-};
+}
 
 export default Expenses;
