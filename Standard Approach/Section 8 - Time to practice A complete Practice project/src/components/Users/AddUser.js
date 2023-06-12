4f6fe7395754f263646f49b0b8333379226208f8
Shashwat Singh <shashwat2048@gmail.com>
2023-06-12 19:38:54 +0530

    93. Adding VAlidation & Resetting

--- Standard Approach/Section 8 - Time to practice A complete Practice project/src/components/Users/AddUser.js
+++ Standard Approach/Section 8 - Time to practice A complete Practice project/src/components/Users/AddUser.js
@@ -9,8 +9,22 @@ const AddUser = (props) => {
   const [enteredAge, setEnteredAge] = useState("");
 
   const addUserHandler = (event) => {
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredAge.trimEnd().length === 0
+    ) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      // '+' - here is used for forced conversion to number for comparision.
+      return;
+    }
+
     event.preventDefault();
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
@@ -26,10 +40,20 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
 
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
 
         <Button type="submit">Add User</Button>
       </form>
