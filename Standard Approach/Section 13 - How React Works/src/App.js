345d4a10ae254a97c73479f65ce57f6082e8b061
Shashwat Singh <shashwat2048@gmail.com>
2023-06-19 17:07:47 +0530

    187.A closer Look At Child Component Re-Evaluation, 188. Preventing Unnecessary Re-Evaluations with React.memo(), 189. Preventing Function ReCreation with useCallback()

--- Standard Approach/Section 13 - How React Works/src/App.js
+++ Standard Approach/Section 13 - How React Works/src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import Button from "./components/UI/Button/Button";
 
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  const toggleParagtaphHandler = () => {
+  console.log("App");
+
+  const toggleParagtaphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagtaphHandler}>Toggle Paragraph!</Button>
     </div>
   );
