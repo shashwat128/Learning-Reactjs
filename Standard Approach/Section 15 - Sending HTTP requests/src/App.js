b92eda59a5dcc79a85b39678fc7cb6548f4b14ab
Shashwat Singh <shashwat2048@gmail.com>
2023-06-21 11:54:40 +0530

    210. Sending a GET Request

--- Standard Approach/Section 15 - Sending HTTP requests/src/App.js
+++ Standard Approach/Section 15 - Sending HTTP requests/src/App.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: "John Wick",
-      openingText: "This movie is of garbage value.",
-      releaseDate: "2023-06-18",
-    },
-    {
-      id: 2,
-      title: "Spiderman",
-      openingText: "A man who swings using strings.",
-      releaseDate: "2023-06-25",
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
