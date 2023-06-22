ed231fb6b2cccfdcc5cad4c2895ae81814e7d128
Shashwat Singh <shashwat2048@gmail.com>
2023-06-22 13:53:38 +0530

    225. Building a Custom Http Hook

---
+++ Standard Approach/Section 16 - Custom Hooks/Project 2 - More Realistic/src/hooks/use-http.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequests = async (taskText) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendRequests: sendRequests,
+    /*in this as the names are kept same,
+    it could siplmy be written as
+    {   isLoading,
+        error,
+        sendRequests, }
+    in modern javaScript
+     */
+  };
+};
+
+export default useHttp;
