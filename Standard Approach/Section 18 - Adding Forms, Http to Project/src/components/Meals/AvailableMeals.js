7a6b40b394ccd358d9a16051443f89d6f031267e
Shashwat Singh <shashwat2048@gmail.com>
2023-06-24 12:02:40 +0530

    248.Moving 'Meals' Data to the Backend, 249. Fetching Meals via Http

---
+++ Standard Approach/Section 18 - Adding Forms, Http to Project/src/components/Meals/AvailableMeals.js
@@ -0,0 +1,53 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+
+  //useEffect cant be async function (not allowed) so it is nested inside it to mask it
+  //thus overall useEffect function does not return a promise
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-http-74e3d-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
