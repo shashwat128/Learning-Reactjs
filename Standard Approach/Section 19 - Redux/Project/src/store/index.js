2154f70a338bfb7b9495444be8cc80f9ca980e07
Shashwat Singh <shashwat2048@gmail.com>
2023-06-25 17:47:12 +0530

    267. Creating a Redux Store for React

---
+++ Standard Approach/Section 19 - Redux/Project/src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+//existing state as the first argument, asction dispatched as second
+//state given default value, so that when the reducer is executed for the first time, we have an initial state.
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+
+  return state;
+};
+
+//central data (state) store
+//create store wants a pointer at a resucer function as a parametre.
+const store = createStore(counterReducer);
+
+export default store;
