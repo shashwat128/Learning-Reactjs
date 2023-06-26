a0988a9a5e32a16f24be691e4e9468a896f1e0bb
Shashwat Singh <shashwat2048@gmail.com>
2023-06-26 20:14:20 +0530

    279. Working eith Multiple Slices

--- Standard Approach/Section 19 - Redux/Project/src/components/Counter.js
+++ Standard Approach/Section 19 - Redux/Project/src/components/Counter.js
@@ -5,42 +5,23 @@ import { counterActions } from "../store";
 const Counter = () => {
   const dispatch = useDispatch();
 
-  //The function will get access to our store using useSelector();
-  //We need to pass a function to useSelector -it will be executed by react-Redux
-  //This function will decide which piece of data we want to extract from our store.
-  //the passed function here will recieve a state managed by redux and then we return part of the state which we want to extract
-  const counter = useSelector((state) => state.counter);
-  //when we use "useSelector" - react redux will automatically set up a subscription to the Redux store for this component.
-  //any change will trigger it - which brings the latest state.
+  const counter = useSelector((state) => state.counter.counter);
 
-  //ToggleCounter - visibility
-  const show = useSelector((state) => state.showCounter);
+  const show = useSelector((state) => state.counter.showCounter);
 
-  //increment and decrement handler function using the dispatch function
   const incrementHandler = () => {
-    //the identifiers should be exactly what was passed inside the conditional state in the reducer function in the "store/index.js". (no typos!)
-    //dispatch({ type: "increment" });
-
     dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    //dispatch({ type: "increase", amount: 5 });
-
-    //passing payload data
-    //stored in extra field name "payload: 5"
     dispatch(counterActions.increase(5));
   };
 
   const decrementHandler = () => {
-    //dispatch({ type: "decrement" });
-
     dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    //dispatch({ type: "toggle" });
-
     dispatch(counterActions.toggleCounter());
   };
 
@@ -59,52 +40,3 @@ const Counter = () => {
 };
 
 export default Counter;
-
-/*
-=== Class based component
-class Counter extends Component {
-  incrementHandler() {
-    this.props.increment();
-  }
-
-  decrementHandler() {
-    this.props.decrement();
-  }
-
-  toggleCounterHandler() {}
-
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-        </div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    );
-  }
-}
-
-//connect will return a new function as a value, which we then execute again, and then we pass out component to that returned function as our argument.
-//connect will take two arguments and both are functions
-//first function maps redux state to props, which will be recieved by this Counter component then.
-//it will return an object where keys will be returned as props in the receiving component.(counter component)
-//Values of those keys are the logic drilling into the redux state.
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increment: () => dispatch({ type: "increment" }),
-    decrement: () => dispatch({ type: "decrement" }),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
-*/
--- Standard Approach/Section 19 - Redux/Project/src/store/index.js
+++ Standard Approach/Section 19 - Redux/Project/src/store/index.js
@@ -1,29 +1,19 @@
 import { createStore } from "redux";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-//existing state as the first argument, action dispatched as second
-//state given default value, so that when the reducer is executed for the first time, we have an initial state.
+const initialCounterState = { counter: 0, showCounter: true };
 
-const initialState = { counter: 0, showCounter: true };
-
-//Object as an argument.
 const counterSlice = createSlice({
-  name: "counter", //identifier
-  //all the methods inside reducer will be called by Redux and will automatically receive the current state
-  initialState: initialState, //initial state
+  name: "counter",
+  initialState: initialCounterState,
   reducers: {
-    // we wont need action here because these methods will automatically be called depending on which action was triggered
-    //We are allowed to mutate the state here
-    //because redux toolkit internally uses another package "imgur", which detects codes like this and clone the existing state,  create a new state object, keep all the state which we are not editing, and override the state which we are editing in an immutable way
     increment(state) {
       state.counter++;
     },
     decrement(state) {
       state.counter--;
     },
-    //for this we need extra data
     increase(state, action) {
-      //state.counter = state.counter + action.amount;
       state.counter = state.counter + action.payload;
     },
     toggleCounter(state) {
@@ -32,65 +22,34 @@ const counterSlice = createSlice({
   },
 });
 
-/*counterReducer old (without toolkit)
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
+const initialAuthState = {
+  isAuthenticated: false,
+};
 
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
 
-      //Never mutate the current state always override - could lead to unexpected bugs and side-effects (state get out of sync, and suddenly the UI is not reflecting your state)
-      //state.counter++   --   XXX
-    };
-  }
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === "toggle") {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-}; 
-
-//central data (state) store
-//create store wants a pointer at a resucer function as a parametre.
-const store = createStore(counterReducer);
-*/
-
-/* redux solution
-const store = createStore(counterSlice.reducer);
-//this is a good to go solution for smaller application but if the application goes bigger then it will grow in multiple state slices too and we would face problem with this solution. 
-//because there could only be one reducer be passed to create storeand when we have multiple slices, we have multiple reducers which we access with .reducer
-*/
-
-/*
-//central data (state) store
-//create store wants a pointer at a resucer function as a parametre.
-const store = createStore(counterReducer);
-*/
-
-//main reducer for configureStore
-//an object could be passed inside it as key which could be used to pass multiple reducers thereafter.
+//now we pass map of objects as value to reducer
+//this is how we combine multiple slices and their reducers
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
