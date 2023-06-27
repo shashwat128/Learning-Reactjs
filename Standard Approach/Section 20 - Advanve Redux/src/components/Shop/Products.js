798e564fba5f99cea26557fb2ad67e37fe8da0ad
Shashwat Singh <shashwat2048@gmail.com>
2023-06-27 16:35:42 +0530

    287. Refresher / Practice: Part 2

--- Standard Approach/Section 20 - Advanve Redux/src/components/Shop/Products.js
+++ Standard Approach/Section 20 - Advanve Redux/src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "b1",
+    price: 12.98,
+    title: "Harry Potter",
+    description: "Jhadu pe udne vala, jadui chashmish ladka.",
+  },
+  {
+    id: "b2",
+    price: 8.51,
+    title: "Intelligent Investor",
+    description: "Dimag lagayen, paisa kamayen.",
+  },
+  {
+    id: "b3",
+    price: 10.37,
+    title: "Rich Dad, Poor Dad!",
+    description: "Paisa banayen na ki gavayen.",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
