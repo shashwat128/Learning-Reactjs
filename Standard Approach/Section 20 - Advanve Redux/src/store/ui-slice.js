f7b043f5c935f032e16f5689a7dbc146ff784c24
Shashwat Singh <shashwat2048@gmail.com>
2023-06-29 13:49:47 +0530

    293. Handling Http States & Feedback with Redux

--- Standard Approach/Section 20 - Advanve Redux/src/store/ui-slice.js
+++ Standard Approach/Section 20 - Advanve Redux/src/store/ui-slice.js
@@ -2,11 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui",
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false, notification: null },
   //map of method that represent all the different cases, different actions we want to handle with this reducer
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
