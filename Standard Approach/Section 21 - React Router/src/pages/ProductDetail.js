fb329784f8153653c6b88d3e51ee88ff912d608d
Shashwat Singh <shashwat2048@gmail.com>
2023-07-03 12:17:37 +0530

    313. Understanding Relative and Absolute Paths

--- Standard Approach/Section 21 - React Router/src/pages/ProductDetail.js
+++ Standard Approach/Section 21 - React Router/src/pages/ProductDetail.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ProductDetailPage() {
   const params = useParams();
@@ -7,6 +8,11 @@ function ProductDetailPage() {
     <>
       <h1>Product Details!</h1>
       <p>{params.productId}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
     </>
   );
 }
