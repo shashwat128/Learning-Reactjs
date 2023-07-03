fe6d7a91adc38d6e14f4a2077d1baccf1fa056a3
Shashwat Singh <shashwat2048@gmail.com>
2023-07-03 21:31:20 +0530

    320. More Loader() Data usage, 321. Where should loader() code be stored?, 322. When are loader() Functions Executed?

--- Standard Approach/Section 21 - React Router/12-adv-starting-project/frontend/src/App.js
+++ Standard Approach/Section 21 - React Router/12-adv-starting-project/frontend/src/App.js
@@ -24,7 +24,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage from "./pages/EventDetails";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
@@ -45,16 +45,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-
-              if (!response.ok) {
-                //response - later
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
