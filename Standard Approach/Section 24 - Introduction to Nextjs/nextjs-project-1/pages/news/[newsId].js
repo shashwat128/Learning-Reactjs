e2f6c8519e46f7c99f3d1d094a9c23457e05baa9
Shashwat Singh <shashwat2048@gmail.com>
2023-08-05 11:57:00 +0530

    377. Extracting Dynamic Parameter

--- Standard Approach/Section 24 - Introduction to Nextjs/nextjs-project-1/pages/news/[newsId].js
+++ Standard Approach/Section 24 - Introduction to Nextjs/nextjs-project-1/pages/news/[newsId].js
@@ -1,6 +1,12 @@
+import { useRouter } from "next/router";
+
 //our-domaim.com/news/something-important
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
   return <h1>Something-Important Page.</h1>;
 }
 
